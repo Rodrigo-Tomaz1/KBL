9f8403bc437827a82f48d899180a55dc64f44f59
Rodrigo <rodpay10@gmail.com>
2023-10-12 00:33:49 -0300

    Atualização 04 - Projeto KBL

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const menuMobile = document.querySelector('.menu-mobile');
+const menuMobile = document.querySelector('.header-content-mobile');
 
-    function expandir() {
-        if (menuMobile.classList.contains('ativo')) {
-            menuMobile.classList.remove('ativo');
-            document.querySelector('.icon').src = "CSS/header/header-mobile/btn-menu/expandir.svg"
-        } else {
-            menuMobile.classList.add('ativo')
-            document.querySelector('.icon').src = "CSS/header/header-mobile/btn-menu/retrair.svg"
-        }
-    }
+function expandir() {
+    const menuMobile = document.querySelector('.header-content-mobile');
+   if (menuMobile.classList.contains('ativo')) {
+        menuMobile.classList.remove('ativo');
+        document.querySelector('.icon').src = 'CSS/header/header-mobile-icons/abrir.svg'
+    } else {
+        menuMobile.classList.add('ativo')
+        document.querySelector('.icon').src = 'CSS/header/header-mobile-icons/fechar.svg'
+   }
+}
 
-const efeitoMenuDesktop = document.querySelector('.header')
+// const efeitoMenuDesktop = document.querySelector('.header')
 
-window.addEventListener('scroll', function() {
-    efeitoMenuDesktop.classList.toggle('rolagem', this.window.scrollY > 0);
-})
+// window.addEventListener('scroll', function() {
+//     efeitoMenuDesktop.classList.toggle('rolagem', this.window.scrollY > 0);
+// })
 
